d9c8d375d6bbf71bb2b44f076bb176afb467f540
Erdij <yildirimsukruerdi@gmail.com>
2022-10-26 02:51:06 +0300

    first_route

---
+++ src/components/App.js
@@ -0,0 +1,50 @@
+import React from "react";
+import MovieList from "./MovieList";
+import SearchBar from "./SearchBar";
+import AddMovie from "./AddMovie";
+
+import axios from "axios";
+
+class App extends React.Component {
+  state = {
+    movies: [],
+    searchQuery: "",
+  };
+
+  async componentDidMount() {
+    const response = await axios.get("http://localhost:3002/movies");
+    this.setState({ movies: response.data });
+  }
+
+  // AXIOS API
+  deleteMovie = async (movie) => {
+    axios.delete(`http://localhost:3002/movies/${movie.id}`);
+    const newMovieList = this.state.movies.filter((m) => m.id !== movie.id);
+    this.setState((state) => ({
+      movies: newMovieList,
+    }));
+  };
+
+  searchMovie = (event) => {
+    this.setState({ searchQuery: event.target.value });
+  };
+
+  render() {
+    let filteredMovies = this.state.movies.filter((movie) => {
+      return movie.name.toLowerCase().indexOf(this.state.searchQuery) !== -1;
+    });
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-12">
+            <SearchBar searchMovieProp={this.searchMovie} />
+          </div>
+        </div>
+        <MovieList movies={filteredMovies} deleteMovieProp={this.deleteMovie} />
+        <AddMovie />
+      </div>
+    );
+  }
+}
+
+export default App;
